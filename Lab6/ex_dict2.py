92058d34919d97d680329f6ebc74385f6329ea6c
Phatchadawadee <phatchadawadee.s@rmutsvmail.com>
2022-01-25 11:09:41 +0700

    Lab 6 : dictionary

---
+++ Lab6/ex_dict2.py
@@ -0,0 +1,20 @@
+# Copy Dictionary
+
+x = {1:'Phatchadawadee',2:'Sariphat',3:'RUTS'}
+print(x)
+print(x.keys())
+
+# copy()
+y = x
+print(y)
+y[3] = 'Saiyai'
+print(y)
+print(x)
+
+y = x.copy()
+print(x)
+print(y)
+y[1] = 'Nattapong'
+y[2] = 'Kaewboonma'
+print("After changing")
+
